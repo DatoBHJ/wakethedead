25327bcdcea1c77067a6c6636c34134f1995d387
datobhj <bdfutbol@naver.com>
2024-09-24 16:26:04 +0900

    asf

--- components/answer/InitialQueries.tsx
+++ components/answer/InitialQueries.tsx
@@ -12,26 +12,54 @@ const InitialQueries: React.FC<InitialQueriesProps> = ({ questions, handleFollow
   };
 
   const formatSummary = (summary: string) => {
-    // Remove '**' from the beginning and end of the summary
     return summary.replace(/^\*\*|\*\*$/g, '');
   };
+
+  const renderItem = (item: string, index: number) => {
+    const isSummary = item.startsWith('**Part');
+    const formattedItem = isSummary ? formatSummary(item) : item;
+
+    return (
+      <li
+        key={index}
+        className="flex items-center cursor-pointer bg-transparent p-3 my-1"
+        onClick={() => handleQuestionClick(item)}
+      >
+        <span role="img" aria-label="link" className="mr-2 text-blue-500 dark:text-blue-400 hover:text-blue-600 dark:hover:text-blue-300">
+          <IconPlus className='flex h-5 w-5 pb-1' />
+        </span>
+        <p className={`${isSummary ? 'font-handwriting font-bold text-base md:text-lg' : 'font-handwriting text-sm md:text-base '} dark:text-white text-black`}>
+          {formattedItem}
+        </p>
+      </li>
+    );
+  };
+
+  const renderSeparator = (index: number) => (
+    <li key={`separator-${index}`} className="my-2">
+      <hr className="w-full h-px bg-gray-200 dark:bg-gray-700 opacity-50" />
+    </li>
+  );
+
+  const groupedItems = questions.reduce((acc, item, index) => {
+    if (item.startsWith('**Part')) {
+      acc.push([item]);
+    } else if (acc.length > 0) {
+      acc[acc.length - 1].push(item);
+    } else {
+      acc.push([item]);
+    }
+    return acc;
+  }, [] as string[][]);
 
   return (
     <div className="max-h-[calc(100vh-300px)] overflow-y-auto">
       <ul className="mt-2 md:mr-10 mr-0">
-        {questions.map((question, index) => (
-          <li
-            key={index}
-            className="flex items-center cursor-pointer bg-transparent p-3 my-1"
-            onClick={() => handleQuestionClick(question)}
-          >
-            <span role="img" aria-label="link" className="mr-2 text-blue-500 dark:text-blue-400 hover:text-blue-600 dark:hover:text-blue-300">
-              <IconPlus className='flex h-5 w-5 pb-1' />
-            </span>
-            <p className={`${question.startsWith('**Part') ? 'font-handwriting font-bold text-base md:text-lg' : 'font-handwriting text-sm md:text-base '} dark:text-white text-black`}>
-              {question.startsWith('**Part') ? formatSummary(question) : question}
-            </p>
-          </li>
+        {groupedItems.map((group, groupIndex) => (
+          <React.Fragment key={groupIndex}>
+            {group.map(renderItem)}
+            {groupIndex < groupedItems.length - 1 && renderSeparator(groupIndex)}
+          </React.Fragment>
         ))}
       </ul>
     </div>
