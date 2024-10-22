e341437fe0356c4e478e6e7995144b6bc43d839d
datobhj <bdfutbol@naver.com>
2024-10-23 03:21:17 +0900

    asdf

--- components/ThemeBasedVideo.tsx
+++ components/ThemeBasedVideo.tsx
@@ -3,20 +3,41 @@ import React from 'react';
 const ThemeBasedVideo = () => {
   const videoSource = '/videos/grey_mars_c_blue_1920.mp4';
 
+  // const [showDescription, setShowDescription] = useState(false);
+
+  // const handleClick = () => {
+  //   setShowDescription(!showDescription);
+  // };
+
   return (
-    <div className="fixed inset-0 w-full h-full -z-10">
-      <div className="absolute inset-0 bg-black/30" /> {/* Optional overlay for better contrast */}
-      <video 
-        preload="none"
-        autoPlay 
-        loop  
-        muted 
-        playsInline
-        className="w-full h-full object-cover"
+    <div className='flex relative justify-center sm:block'>
+      <div 
+        className="w-80 h-80 p-2 sm:w-60 sm:h-60 md:w-80 md:h-80 max-w-80 max-h-80 border-none relative"
+        // className="border-none relative"
+        // onClick={handleClick}
       >
-        <source src={videoSource} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+        <video 
+          preload="none"
+          autoPlay 
+          loop  
+          muted 
+          playsInline
+          className="w-full h-full object-cover rounded-full non-selectable"
+        >
+          <source src={videoSource} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        {/* {showDescription && (
+          <div className="absolute inset-0 bg-black bg-opacity-70 rounded-full flex items-center justify-center p-4">
+            <p className="text-white text-center text-xs pb-10">
+              Paste your link, generate handwritten-style notes
+              Learn and explore with shared content via chat!<br/>🌍🤖<br/>
+              Share knowledge in seconds - paste a link or click links from chat responses!<br/><br/>
+              Join our quick, fun learning revolution! 🚀📚
+            </p>
+          </div>
+        )} */}
+      </div>
     </div>
   );
 };
