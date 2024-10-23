9acf49fff9762dcdfc072572a677ff514cbb761c
datobhj <bdfutbol@naver.com>
2024-10-23 19:04:04 +0900

    asdf

--- components/answer/ExtractedQuestionsModal.tsx
+++ components/answer/ExtractedQuestionsModal.tsx
@@ -19,7 +19,7 @@ const ExtractedQuestionsModal: React.FC<ExtractedQuestionsModalProps> = ({
   const isDesktop = useMediaQuery("(min-width: 1024px)");
 
   return (
-    <div className="bg-background dark:bg-background overflow-auto h-full">
+    <div className="overflow-auto h-full">
       <div className={`max-w-4xl mx-auto p-4 ${isDesktop ? 'py-10' : 'py-4'} h-full flex flex-col`}>
         <div className="flex-grow flex items-center justify-center">
           <InitialQueries 
